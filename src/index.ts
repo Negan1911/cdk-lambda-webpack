ebbe6601148f62ce76ac283a666a7e1d2a3089b9
Nahuel Veron <nahuelav96@gmail.com>
2022-04-09 03:39:34 -0300

    fix: Remove custom zipper

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import path from 'path'
 import _ from 'lodash'
 import { Construct } from 'constructs'
-import { zipDirectory } from './zipper'
 import { packModules } from './modules'
 import { Yarn2 } from './packagers/yarn2'
 import { webpackCompile } from './compiler'
@@ -24,28 +23,27 @@ export async function LambdaWebpack(scope: Construct, id: string, { webpack, han
   const zipId = uid()
   const _cwd = cwd || process.cwd()
   const config = require(path.isAbsolute(webpack.webpackConfigPath) ? webpack.webpackConfigPath : path.join(process.cwd(), webpack.webpackConfigPath))
-  const buildFolder = path.join(_cwd, '.build')
-  const buildPath = path.join(buildFolder, id)
+  
+  const buildFolder = await xfs.mktempPromise()
   const [entry, webpackEntry, exportName] = getEntry(handler, _cwd)
-  const _config = typeof config === 'function' ? config({ id, buildPath, webpackEntry, buildFolder, entry }) : (config || {})
+  const _config = typeof config === 'function' ? config({ id, buildPath: buildFolder, webpackEntry, buildFolder, entry }) : (config || {})
   
   const stat = await webpackCompile({
     ..._config,
     entry,
     output: {
         ...(_config.output || {}),
-        path: buildPath,
+        path: buildFolder,
         filename: `${webpackEntry}.js`
     }
   })
   
-  await packModules(stat, id, webpack, buildPath)
-  await zipDirectory(buildPath, buildFolder, zipId)
+  await packModules(stat, id, webpack, buildFolder)
   
   return new lambda.Function(scope, id, {
     ...props,
     handler: `${webpackEntry}.${exportName}`,
-    code: lambda.Code.fromAsset(path.join(process.cwd(), '.build', `${zipId}.zip`))
+    code: lambda.Code.fromAsset(buildFolder)
   })
 }
 
@@ -64,13 +62,10 @@ interface LambdaWebpackYarn2Props extends Omit<lambda.FunctionProps, 'code'> {
  * Experimental Yarn2 support.
  */
 export async function LambdaWebpackYarn2(scope: Construct, id: string, { webpack, handler, cwd, ...props }: LambdaWebpackYarn2Props) {
-  const zipId = uid()
   const _cwd = cwd || process.cwd()
   const config = require(path.isAbsolute(webpack.webpackConfigPath) ? webpack.webpackConfigPath : path.join(process.cwd(), webpack.webpackConfigPath))
 
   const buildFolder = await xfs.mktempPromise()
-  const distFolder = path.join(_cwd, 'cdk.out')
-  
   const [entry, webpackEntry, exportName] = getEntry(handler, _cwd)
   const _config = typeof config === 'function' ? config({ id, buildPath: buildFolder, webpackEntry, buildFolder, entry }) : (config || {})
 
@@ -85,11 +80,10 @@ export async function LambdaWebpackYarn2(scope: Construct, id: string, { webpack
   })
 
   await new Yarn2(buildFolder, _cwd).install()
-  await zipDirectory(buildFolder, distFolder, zipId)
   
   return new lambda.Function(scope, id, {
     ...props,
     handler: `${webpackEntry}.${exportName}`,
-    code: lambda.Code.fromAsset(path.join(distFolder, `${zipId}.zip`))
+    code: lambda.Code.fromAsset(buildFolder)
   })
 }
